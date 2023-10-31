b1453589763d03c66508d9f2d586500cea65d2f5
luisE011 <siulpalac@gmail.com>
2023-10-30 18:04:15 -0600

    build(auth): agrega toda la API de auth

---
+++ auth/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+Route::middleware(['auth:sanctum'])->group(function (){
+
+    Route::get('logout', [AuthController::class, 'logout']);
+});
+
+
+
+
+
+
+Route::middleware('auth:sanctum')->get('/username', function (Request $request) {
+    return $request->user();
+});
