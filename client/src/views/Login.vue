b19f052b369fe1947d0a8950399f2e831f682f51
unknown <ronnhy7@gmail.com>
2023-11-10 04:54:04 -0600

    creacion de la interfaz de login y singup

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,6 +1,8 @@
+
+
 <script setup>
 import { onMounted, ref } from 'vue';
-
+import Header from '../components/Header.vue'
 
 onMounted(() => {
   document.title = 'VC | Login';
@@ -11,9 +13,52 @@ onMounted(() => {
 
 
 <template>
-  <div class="login view-content">
-    <h1>LOGIN</h1>
-  </div>
+  <main>
+    <img src="../assets/img/Logo Voz Ciudadana.svg" alt="" width="120" height="120">
+    <h1>Iniciar Sesion</h1>
+    <form action="">
+      <input type="text" name="" id="" placeholder="Email">
+      <input type="text" name="" id="" placeholder="Password">
+      <input type="submit" value="Iniciar Sesion">
+    </form>
+    <p>No tienes cuenta?, Registrate</p>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main{
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  margin: 0 auto;
+}
+h1, p{
+  text-align: center;
+}
+
+h1{
+  font-size: 3rem;
+}
+
+form{
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+img{
+  margin: 0 auto;
+}
+
+input[type = "submit"]{
+  background: #022E40;
+  color: #ffffff;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 1.5rem;
+}
+</style>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -10,11 +10,53 @@ onMounted(() => {
 
 
 <template>
-    <div class="signup view-content">
-        
-    </div>
+  <main>
+    <img src="../assets/img/Logo Voz Ciudadana.svg" alt="" width="120" height="120">
+    <h1>Registrarse</h1>
+    <form action="">
+      <input type="text" name="" id="" placeholder="Usuario">
+      <input type="text" name="" id="" placeholder="Email">
+      <input type="text" name="" id="" placeholder="Password">
+      <input type="submit" value="Registrarme">
+    </form>
+    <p>Ya tienes cuenta?, Inicia Sesion</p>
+  </main>
 </template>
 
 <style lang="scss" scoped>
-    
+main{
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  margin: 0 auto;
+}
+h1, p{
+  text-align: center;
+}
+
+h1{
+  font-size: 3rem;
+}
+
+form{
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+img{
+  margin: 0 auto;
+}
+
+input[type = "submit"]{
+  background: #022E40;
+  color: #ffffff;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 1.5rem;
+}
 </style>
