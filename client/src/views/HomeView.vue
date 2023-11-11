cd928a31f0fdd66470dffd66a7db1e3524f45c66
unknown <ronnhy7@gmail.com>
2023-11-10 19:15:00 -0600

    creacion de la interfaz de home

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -10,10 +10,47 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="home view-content">
-    <Header />
-    <router-link to="/login">Iniciar Sesión</router-link>
-  </div>
+  <Header />
+  <main>
+    <h1>Bienvenidos</h1>
+    <img src="../assets/img/Logo Voz Ciudadana.svg" alt="" width="170" height="170">
+    <router-link class="btn-start" to="/login">Comenzar</router-link>
+    <p>LIFO</p>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 100vh;
+  width: 90%;
+  margin: 0 auto;
+}
+
+.btn-start{
+  background: #022E40;
+  color: #ffffff;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 1.5rem;
+  width: 60%;
+  margin: 0 auto;
+  text-align: center;
+}
+
+img{
+  margin: 0 auto;
+}
+
+h1{
+  text-align: center;
+}
+
+p{
+  font-weight: lighter;
+  letter-spacing: 10px;
+  text-align: center;
+}
+</style>
