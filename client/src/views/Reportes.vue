d327dbdd900c76c5ea6aa3fcd5b5926af4ecf88f
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2023-11-14 00:01:24 -0600

    reportes avance

--- client/src/views/Reportes.vue
+++ client/src/views/Reportes.vue
@@ -1,9 +1,20 @@
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <template>
-<h1>Reportes</h1>
+  <div class="container">
+    <div class="titulo">
+      <h2>Reportes</h2>
+      <div class="card">
+        <div class="card-header">13/11/2023</div>
+        <div class="card-body">
+          <h4 class="card-title">Reporte 1</h4>
+          <img src=".." class="rounded float-end" alt="...">
+          <p class="card-text">Text</p>
+        </div>
+        <div class="card-footer text-muted">11:54 am</div>
+      </div>
+    </div>
+  </div>
 </template>
